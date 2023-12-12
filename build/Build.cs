d80bd6fd551c1505c9bd13b44490a2d89f730b91
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2023-12-12 12:23:57 +1000

    Use .NET 8

--- build/Build.cs
+++ build/Build.cs
@@ -28,7 +28,7 @@ class Build : NukeBuild
     [Parameter("Branch name for OctoVersion to use to calculate the version number. Can be set via the environment variable " + CiBranchNameEnvVariable + ".", Name = CiBranchNameEnvVariable)]
     string BranchName { get; set; }
     
-    [OctoVersion(BranchMember = nameof(BranchName), AutoDetectBranchMember = nameof(AutoDetectBranch), Framework = "net6.0")]
+    [OctoVersion(BranchMember = nameof(BranchName), AutoDetectBranchMember = nameof(AutoDetectBranch))]
     public OctoVersionInfo OctoVersionInfo;
     
     static AbsolutePath SourceDirectory => RootDirectory / "source";
